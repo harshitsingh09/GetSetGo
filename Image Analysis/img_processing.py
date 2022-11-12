3e92927c2c74aacd372c094716e9de0da4e51aa2
Saravana200 <dhruvassaravana@gmail.com>
2022-11-12 15:28:27 +0530

    bg_subu

--- Image Analysis/img_processing.py
+++ Image Analysis/img_processing.py
@@ -3,37 +3,35 @@ from random import randint
 import cv2 as cv
 import numpy as np
 
-vid=cv.VideoCapture("C:\\Users\\Dell\\Documents\\GitHub\\GetSetGo\\Image Analysis\\tf.mp4")
+vid=cv.VideoCapture("C:\\Users\\Dell\\Documents\\GitHub\\GetSetGo\\Image Analysis\\tufu.mp4")
 i=0
 total=0
+bg=cv.createBackgroundSubtractorMOG2(history=4,detectShadows=False)
 while True:
     ret, frame=vid.read()
-    ret, frame2=vid.read()
-    frame=cv.resize(frame,(240,320))
-    frame2=cv.resize(frame2,(240,320))
+    frame=cv.resize(frame,(280,420))
     i+=1
     new_img=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    new_img2=cv.cvtColor(frame2,cv.COLOR_BGR2GRAY)
-    new_img=cv.absdiff(new_img,new_img2)
-    ret,new_img=cv.threshold(new_img,30,250,cv.THRESH_BINARY)
-    kernel=np.ones((5,5),int)
-    new_img=cv.dilate(new_img,kernel,iterations=1)
-    cont,_=cv.findContours(new_img,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+    kernel=np.ones((3,3),dtype="uint8")
+    ret,new_img=cv.threshold(new_img,30,255,cv.THRESH_BINARY)
+    new_img=bg.apply(new_img)
+    new_img=cv.erode(new_img,kernel,iterations=1)
+    new_img=cv.dilate(new_img,kernel,iterations=2)
+    cont,_=cv.findContours(new_img,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+    frame_copy=frame.copy()
     for c in cont:
         peri=cv.arcLength(c,True)
-        approx=cv.approxPolyDP(c,peri*0.035,True)
-        #print(total)
-        if cv.contourArea(c)>=90 and len(approx)==5:
+        approx=cv.approxPolyDP(c,peri*0.02,True)
+        print(total)
+        if cv.contourArea(c)>400 and len(approx):
             x,y,w,h = cv.boundingRect(c)
-            print(x,y)
+            cv.rectangle(frame_copy,(x,y),(x+w,y+h),(0,255,0),2)
             if(x<=200 and y>=80):   
                 total+=1
-                cv.drawContours(frame,[approx],-1,(0,255,0),1)    
-                cv.imwrite("C:\\Users\\Dell\\Documents\\GitHub\\GetSetGo\\Image Analysis\\im_test\\"+str(i)+".jpeg",frame)
-        #cv.waitKey(2000)
-          
-    cv.imshow('test',frame) 
-     
+    fore_grnd=cv.bitwise_and(frame,frame,mask=new_img)
+    stacked=np.hstack((frame,fore_grnd,frame_copy))
+    cv.imwrite("C:\\Users\\Dell\\Documents\\GitHub\\GetSetGo\\Image Analysis\\im_test\\"+str(i)+".jpeg",stacked)
+    cv.imshow('test',stacked) 
     if cv.waitKey(1)==ord('q'):
       break
 
